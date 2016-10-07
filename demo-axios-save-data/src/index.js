19c5bdee1b69c6f03807095658dbf0d36df281f9
rong12345 <1165895564@qq.com>
2016-10-07 17:00:14 +0800

    finish

--- demo-axios-save-data/src/index.js
+++ demo-axios-save-data/src/index.js
@@ -1,51 +1,38 @@
 var express = require('express');
-var  app = express();
-var bodyParser = require('body-parser');
+
+var app = express();
 var mongoose = require('mongoose');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
 
-var cors = require('cors');
-app.use(cors());
-
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/request');
-
-var Post = require('./models/post');
+mongoose.connect('mongodb://localhost:27017/save');
 
 var db = mongoose.connection;
 db.on('error', console.log);
 db.once('open', function() {
-  console.log('success!')
-}); 
+  console.log(' success!')
+});
 
-app.get('/posts', function(req, res) {
-  Post.find().exec(function(err, posts) {
-    res.json({ posts: posts})
-  });
-})
-app.get('/post/:id', function(req, res) {
-  Post.findOne({_id:req.params.id},function(err,doc) {
-    if (err) return res.send('出错了');
-    res.json({post: doc})
-  })
-})
+var Schema = mongoose.Schema;
 
-app.post('/posts', function(req, res) {
-  // var post = new Post({title: req.body.title});
-  console.log(req.body);
-  var post = new Post({
-    title:req.body.title,
-    category:req.body.category,
-    content:req.body.content,
-  });
+var PostSchema = new Schema(
+  {
+    title: String,
+    content: String
+  }
+);
+
+var Post = mongoose.model('Post', PostSchema);
+
+
+
+app.post('/posts', function(req, res){
+  console.log('run post.save()');
+  var post = new Post({title:"myTitle", content: "myConent"})
   post.save(function(err){
     if(err) return console.log(err);
-    console.log('save');
-  })
-  // res.redirect('/posts')
-  res.json({message:"成功保存"})
-})
-app.listen(3000, function() {
-  console.log('running on port 3000')
-})
+    console.log('saved');
+  });
+});
+
+app.listen(3000, function(){
+  console.log('running on port 3000.....');
+});
